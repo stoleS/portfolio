2715ce88b03d3c0709b14e657ba42a4beb5f4e52
Predrag <white.knight0857@gmail.com>
2017-08-31 18:37:30 +0200

    UI changes to Admin/Project page

--- routes/admin/projects.js
+++ routes/admin/projects.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: './public/images/portfolio/uploads' })
 var mongo = require('mongodb');
 var db = require('monk')('localhost/myblog');
 
